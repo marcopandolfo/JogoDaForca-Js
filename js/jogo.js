8ba9d24a4fc177959222b3b20bfe1752adbc5442
Marco Antonio Pandolfo <marcopandolfo577@gmail.com>
2019-05-06 01:39:51 -0300

    Func para reiniciar o jogo e verificando se o jogador perdeu ou ganhou

--- js/jogo.js
+++ js/jogo.js
@@ -1,8 +1,35 @@
+/* eslint-disable no-cond-assign */
 // eslint-disable-next-line no-unused-vars
-const criaJogo = () => {
+const criaJogo = (sprite) => {
     let palavraSecreta = '';
-    let lacunas;
+    let lacunas = [];
     let etapa = 1;
+
+    const ganhou = () => (lacunas.length ? !lacunas.some(lacuna => lacuna === '') : false);
+
+    const perdeu = () => sprite.isFinished();
+
+    const ganhouOuPerdeu = () => ganhou || perdeu;
+
+    const reinicia = () => {
+        etapa = 1;
+        lacunas = [];
+        palavraSecreta = '';
+        sprite.reset();
+    };
+
+    const processaChute = (chute) => {
+        const exp = new RegExp(chute, 'gi');
+        let resultado;
+        let acertou = false;
+
+        while (resultado = exp.exec(palavraSecreta)) {
+            lacunas[resultado.index] = chute;
+            acertou = true;
+        }
+
+        if (!acertou) sprite.nextFrame();
+    };
 
     const criaLacunas = () => {
         lacunas = Array(palavraSecreta.length).fill('');
@@ -13,7 +40,7 @@ const criaJogo = () => {
     };
 
     const setPalavraSecreta = (palavra) => {
-        palavraSecreta = palavra.split('');
+        palavraSecreta = palavra;
         criaLacunas();
         proximaEtapa();
     };
@@ -26,5 +53,10 @@ const criaJogo = () => {
         setPalavraSecreta,
         getLacunas,
         getEtapa,
+        processaChute,
+        ganhou,
+        perdeu,
+        ganhouOuPerdeu,
+        reinicia,
     };
 };
