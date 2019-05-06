34ed340b7c38ce35cc9230ae9e5d5974bbbcc868
Marco Antonio Pandolfo <marcopandolfo577@gmail.com>
2019-05-05 23:24:56 -0300

    ESlint

--- js/sprite.js
+++ js/sprite.js
@@ -1,17 +1,17 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable prefer-template */
+// eslint-disable-next-line no-unused-vars
 const createSprite = (seletor) => {
-
     const moveFrame = (from, to) => {
         $el.removeClass('frame' + from);
         $el.addClass('frame' + to);
     };
 
-    const hasNext = () => {
-        return currentFrame + 1 <= 9;
-    };
+    const hasNext = () => currentFrame + 1 <= 9;
 
     const nextFrame = () => {
-        if(hasNext()) {
-            currentFrame++;
+        if (hasNext()) {
+            currentFrame += 1;
             moveFrame((currentFrame - 1), currentFrame);
         }
     };
@@ -21,17 +21,15 @@ const createSprite = (seletor) => {
         currentFrame = 1;
     };
 
-    const isFinished = () => {
-        return !hasNext();
-    };
+    const isFinished = () => !hasNext();
 
     let currentFrame = 1;
     const $el = $(seletor);
     $el.addClass('frame' + currentFrame);
 
     return {
-        nextFrame: nextFrame,
-        reset: reset,
-        isFinished: isFinished,
+        nextFrame,
+        reset,
+        isFinished,
     };
 };
